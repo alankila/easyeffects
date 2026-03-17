1633c788458fb5961b6f48877abe30c74676022a
Antti S. Lankila <alankila@bel.fi>
2026-03-17 21:19:24 +0200

    LCC work forward-ported

--- src/tags_plugin_name.cpp
+++ src/tags_plugin_name.cpp
@@ -40,6 +40,7 @@ Model::Model(QObject* parent)
                 {BaseName::expander, i18n("Expander")},
                 {BaseName::filter, i18n("Filter")},
                 {BaseName::gate, i18n("Gate")},
+                {BaseName::lcc, i18n("Crosstalk Canceller")},
                 {BaseName::levelMeter, i18n("Level Meter")},
                 {BaseName::limiter, i18n("Limiter")},
                 {BaseName::loudness, i18n("Loudness")},
